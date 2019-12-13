83a795de43e08c48ca2f3d8cb0bec542f954739d
DESKTOP-SB6JG5T\十木 <jukisan0525@gmail.com>
2019-12-13 21:12:14 +0800

    change name

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    //游戏初始化
+    private void Init()
+    {
+        
+    }
+}
