9a8ae531cb89c1b86d358eed2cd43a3ad8b718ce
王啸川 <m15048619182@163.com>
2019-12-14 00:36:57 +0800

    moss：加了两个类，readme 的内容加了一个流程图，复制了一个场景做并行开发

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Order
+{
+    public int reward;                  // 报酬
+    public int rewardTime;              // 回报时间
+    public int orderMoney;              // 定金
+    public bool hasOrderMoney;          // 是否拥有定金
+    public float conceptModule;         // 评价影响系数
+    public string[] demands;            // 要求（存疑，或许要建一个 Demand 新类）
+    public float completeDegree;        // 订单完成度
+    public float costModule;            // 成本系数
+    public bool isSpLabel;              // 特殊订单标签
+    public string[] report;             // 订单完成报告
+    public bool isChecked;              // 订单是否完成（完成/超时）？？？
+    public Order()
+    {
+        // TODO：给定外部参数进行订单生成
+    }
+
+}
---
+++ Assets/Scripts/OrderManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // TODO: 根据机制生成的派单函数
+    public Order[] InitOrders(int num)
+    {
+        Order[] orders = new Order[num];
+        for(int i = 0;i < num;i++)
+        {
+            // TODO
+            orders[i] = new Order();
+        }
+        return orders;
+    }
+}
