1f3c283ea19e5b7683bf475d90e1a5cdf05f24f6
王啸川 <m15048619182@163.com>
2019-12-14 17:06:01 +0800

    Order 修改，头像素材

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -57,6 +57,9 @@ public class Order
 
     }
 
-
+    public void SetNextOrderActive()
+    {
+        oc.SetOrderActive(NextOrderID);
+    }
 
 }
--- Assets/Scripts/OrderController.cs
+++ Assets/Scripts/OrderController.cs
@@ -86,6 +86,15 @@ public class OrderController : MonoBehaviour
         return orders.ToArray(); 
     }
 
+    public void SetOrderActive(int id)
+    {
+        orderPool[id].IsActive = true;
+    }
+
+    public void SetOrderDisActive(int id)
+    {
+        orderPool[id].IsActive = false;
+    }
     public int CheckActiveOrder()
     {
         int count = 0;
