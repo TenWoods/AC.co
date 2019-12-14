096177e7a9469ed28268c21ac93f9c9866366e4e
王啸川 <m15048619182@163.com>
2019-12-14 10:57:59 +0800

    no message

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -1,22 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
+using UnityEngine.UI;
 public class Order
 {
-    public int reward;                  // 报酬
-    public int rewardTime;              // 回报时间
-    public int orderMoney;              // 定金
-    public bool hasOrderMoney;          // 是否拥有定金
-    public float conceptModule;         // 评价影响系数
-    public string[] demands;            // 要求（存疑，或许要建一个 Demand 新类）
-    public float completeDegree;        // 订单完成度
-    public float costModule;            // 成本系数
-    public bool isSpLabel;              // 特殊订单标签
-    public string[] report;             // 订单完成报告
-    public bool isChecked;              // 订单是否完成（完成/超时）？？？
-    public Order()
+    public int ID {get;set;}                        // 订单 ID
+    public int Reward {get;set;}                    // 报酬
+    public int RewardTime {get;set;}                // 回报时间
+    public int OrderMoney {get;set;}                // 定金
+    public float ConceptModule {get;set;}          // 评价影响系数
+    
+    public Service[] demands;                       // 要求（存疑，或许要建一个 Demand 新类）
+    public enum demand                              // 根据要求枚举去生成相应的服务
+    {
+        human,
+        cyberhuman,
+        animal,
+        item,
+        vip,
+    }
+    
+
+    public float CostModule {get;set;}            // 成本系数
+    public string[] Report{get;set;}             // 订单完成报告
+
+    public Image Avatar{get;set;}                // 头像
+    public string Name {get;set;}                 // 用户名
+
+    // 默认数值
+    public float CompleteDegree {get;set;}        // 订单完成度
+    public int Level{get;set;}                  // 订单等级
+    public bool IsSpLabel = false;              // 特殊订单标签
+    public bool HasOrderMoney {get;set;}           // 是否拥有定金
+    public bool IsChecked {get;set;}              // 订单是否完成（完成/超时）？？？
+
+    public Order(int id,int reward,int rewardTime,int orderMoney,float conceptModule,float costModule,
+                float completeDegree = -100,
+                bool hasOrderMoney = false,
+                bool isSpLabel = false,
+                bool isChecked = false)
     {
         // TODO：给定外部参数进行订单生成
+        ID = id;
+        Reward = reward;
+        RewardTime = rewardTime;
+        OrderMoney = orderMoney;
+        ConceptModule = conceptModule;
+        CostModule = costModule;
     }
+
+
 
 }
--- Assets/Scripts/OrderController.cs
+++ Assets/Scripts/OrderController.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
+using System.Text;
+using UnityEngine.UI;
 public class OrderController : MonoBehaviour
 {
+    public GameManager gameManager;
+    public Order[] orders;
 
+    // 可能留存一个上回合生成过的订单 ID
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -17,15 +21,29 @@ public class OrderController : MonoBehaviour
         
     }
 
-    // TODO: 根据机制生成的派单函数
-    public Order[] InitOrders(int num)
+    // 根据设置生成的订单
+    public Order[] InitOrders()
     {
-        Order[] orders = new Order[num];
-        for(int i = 0;i < num;i++)
-        {
-            // TODO
-            orders[i] = new Order();
-        }
+        Order[] orders = new Order[1];
+        // ID,Reward,RewardTime,OrderMoney,ConceptModule,CompleteDegree,CostModule,Name
+        orders[0] = new Order(0,1,2,3,4,5);
+        //orders[1] = new Order(1,1,2,3,4,5);
+        //orders[2] = new Order(2,1,2,3,4,5);
+        //orders[3] = new Order(3,1,2,3,4,5);
+        //orders[4] = new Order(4,1,2,3,4,5);
+        //orders[5] = new Order(1,1,2,3,4,5);
+        //orders[6] = new Order(2,1,2,3,4,5);
+        //orders[7] = new Order(3,1,2,3,4,5);
+        //orders[8] = new Order(4,1,2,3,4,5);
+        //orders[9] = new Order(1,1,2,3,4,5);
+        //orders[10] = new Order(2,1,2,3,4,5);
+        //orders[11] = new Order(3,1,2,3,4,5);
+        //orders[12] = new Order(4,1,2,3,4,5);
+        //orders[13] = new Order(1,1,2,3,4,5);
+        //orders[14] = new Order(2,1,2,3,4,5);
+        //orders[15] = new Order(3,1,2,3,4,5);
+        //orders[16] = new Order(4,1,2,3,4,5);
         return orders;
     }
+
 }
